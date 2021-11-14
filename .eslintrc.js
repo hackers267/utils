79a3ffb91464ec6b7e88f0674a26fd959546cd4e
silence_zhpf <silence_zhpf@aliyun.com>
2021-11-14 20:42:45 +0800

    fix(eslint): 💚 修改eslint配置问题
    💚 修改eslint配置不支持typescript的bug
    
    silence_zhpf<silence_zhpf@aliyun.com>

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,6 +3,7 @@ module.exports = {
     browser: true,
     es2021: true,
     node: true,
+    jest: true,
   },
   extends: ["airbnb-base", "prettier"],
   parser: "@typescript-eslint/parser",
@@ -10,8 +11,26 @@ module.exports = {
     ecmaVersion: 13,
     sourceType: "module",
   },
-  plugins: ["@typescript-eslint", "prettier"],
+  plugins: ["@typescript-eslint", "prettier", "import"],
   rules: {
     "prettier/prettier": "error",
+    "import/extensions": [
+      "error",
+      "ignorePackages",
+      {
+        ts: "never",
+        js: "never",
+        jsx: "never",
+        tsx: "never",
+      },
+    ],
+    // "import/prefer-default-export": "warning",
+  },
+  settings: {
+    "import/resolver": {
+      node: {
+        extensions: [".ts"],
+      },
+    },
   },
 };
