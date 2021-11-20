bd04bbbf9644953ae1766463c411413d443028a2
silence_zhpf <silence_zhpf@aliyun.com>
2021-11-20 16:12:01 +0800

    feat(idCard): ✨ 添加身份证校验
    ✨ 添加身份证的有效位校验
    ✨ 添加身份证的位数校验
    ✨ 添加身份证的格式校验
    
    silence_zhpf<silence_zhpf@aliyun.com>

--- src/idCard/index.test.ts
+++ src/idCard/index.test.ts
@@ -1,4 +1,4 @@
-import { calcBirthday, calcSex } from "./index";
+import { calcBirthday, calcSex, verity } from "./index";
 
 describe("calcBirthday", () => {
   it("should be true", () => {
@@ -29,5 +29,25 @@ describe("calcSex", () => {
     const actual: string = "男";
     const result = calcSex(no);
     expect(result).toBe(actual);
+  });
+});
+
+describe("verity", () => {
+  it("should be true", () => {
+    const no: string = "34052419800101001X";
+    const result = verity(no);
+    expect(result).toBeTruthy();
+  });
+
+  it("should be false", () => {
+    const no: string = "340524198001010010";
+    const result = verity(no);
+    expect(result).toBeFalsy();
+  });
+
+  it("should be false lt 18 bit", () => {
+    const no: string = "34052419800101001";
+    const result = verity(no);
+    expect(result).toBeFalsy();
   });
 });
--- src/idCard/index.ts
+++ src/idCard/index.ts
@@ -3,6 +3,8 @@
  * @param no
  * @returns {string}
  */
+import { __, last, map, modulo, pipe, split, sum, zip } from "ramda";
+
 export function calcBirthday(no: string) {
   return no.slice(6, 14);
 }
@@ -16,4 +18,83 @@ export function calcSex(no: string) {
   const char = no.slice(-2, -1);
   const mod = Number(char) % 2;
   return mod === 1 ? "男" : "女";
+}
+
+/**
+ * 校验身份证
+ * @param no
+ * @returns {boolean}
+ */
+export function verity(no: string): boolean {
+  if (!lengthIs18(no)) return false;
+  if (!matchIdCard(no)) return false;
+  return verityCheckDigit(no);
+
+  function verityCheckDigit(str: string) {
+    const array = calcTwoDimensionalArray(str);
+    const total = calcSum(array);
+    const expected = calcCheckDigit(total);
+    const tail = last(str);
+    return expected === tail;
+  }
+
+  /**
+   * 数据在形式上是否符合身份证格式
+   * @param str
+   */
+  function matchIdCard(str: string): boolean {
+    const reg = /^\d{17}[0-9xX]$/;
+    return reg.test(str);
+  }
+
+  function lengthIs18(str: string) {
+    return str.length === 18;
+  }
+
+  function calcCheckDigit(number: number) {
+    const modEleven = modulo(__, 11);
+    const mod = modEleven(number);
+    const checkDigits: string[] = [
+      "1",
+      "0",
+      "X",
+      "9",
+      "8",
+      "7",
+      "6",
+      "5",
+      "4",
+      "3",
+      "2",
+    ];
+    return checkDigits[mod];
+  }
+
+  function calcTwoDimensionalArray(str: string) {
+    const coefficients: number[] = [
+      7, 9, 10, 5, 8, 4, 2, 1, 6, 3, 7, 9, 10, 5, 8, 4, 2,
+    ];
+    const zipCoefficient = zip(coefficients);
+    return zipCoefficient(getArray()(str));
+  }
+
+  function calcSum(twoDimensionalArray: [number, number][]) {
+    const mapMultiply = map(multiply);
+    const data = mapMultiply(twoDimensionalArray);
+    return sum(data);
+  }
+
+  function getArray() {
+    const splitEmpty = split("");
+    const mapToNumber = map(toNumber);
+    return pipe(splitEmpty, mapToNumber);
+  }
+
+  function toNumber(x: string) {
+    return Number(x);
+  }
+
+  function multiply([a, b]: [a: number, b: number]) {
+    return a * b;
+  }
 }
