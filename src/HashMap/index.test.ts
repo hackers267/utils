e90ca8b65f02da3309b43423f5c2c8f658ef928d
silence_zhpf <silence_zhpf@aliyun.com>
2023-04-10 10:33:49 +0800

    chore: 🚧 添加HashMap部分实现

---
+++ src/HashMap/index.test.ts
@@ -0,0 +1,64 @@
+import { HashMap } from "./index";
+
+describe("HashMap has keys", () => {
+  test("has keys", () => {
+    const map = HashMap.new<number, string>();
+    map.insert(1, "one");
+    map.insert(2, "two");
+    expect(map.has(1)).toBeTruthy();
+    expect(map.has(2)).toBeTruthy();
+  });
+
+  test("has no keys", () => {
+    const map = HashMap.new<number, string>();
+    expect(map.has(1)).toBeFalsy();
+    expect(map.has(2)).toBeFalsy();
+  });
+});
+
+describe("HashMap get values", () => {
+  test("get values", () => {
+    const map = HashMap.new<number, string>();
+    map.insert(1, "one");
+    map.insert(2, "two");
+    expect(map.get(1)).toBe("one");
+    expect(map.get(2)).toBe("two");
+  });
+
+  test("get no values", () => {
+    const map = HashMap.new<number, string>();
+    expect(map.get(1)).toBe(undefined);
+    expect(map.get(2)).toBe(undefined);
+  });
+});
+
+describe("HashMap remove keys", () => {
+  test("remove keys", () => {
+    const map = HashMap.new<number, string>();
+    map.insert(1, "one");
+    map.insert(2, "two");
+    const value = map.remove(1);
+    expect(map.has(1)).toBe(false);
+    expect(map.has(2)).toBe(true);
+    expect(value).toBe("one");
+  });
+});
+
+describe("HashMap map", () => {
+  test("map", () => {
+    const map = HashMap.new<number, string>();
+    map.insert(1, "one");
+    map.insert(2, "two");
+    const strings = map.map(([, value]) => value + "!");
+    expect(strings).toEqual(["one!", "two!"]);
+  });
+
+  test("map fn", () => {
+    const map = HashMap.new<number, string>();
+    map.insert(1, "one");
+    map.insert(2, "two");
+    const fn = jest.fn();
+    map.map(fn);
+    expect(fn).toBeCalledTimes(2);
+  });
+});
